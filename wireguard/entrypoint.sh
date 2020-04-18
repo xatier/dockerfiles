3185f4d1aaa7456eecaf5a438ca22bdcdad1a6e3
xatier <xatierlike@gmail.com>
2020-04-18 01:30:38 -0700

    Use my own toy proxy!!

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -6,6 +6,10 @@ set -x
 sudo wg-quick up /srv/wg0.conf
 curl ipinfo.io
 
-sleep 5
+git clone https://github.com/xatier/toy-socks5.git
+cd toy-socks5
+python server.py
 
-/usr/bin/sockd -f /srv/sockd.conf -p /dev/null
+# sleep 5
+# /usr/bin/sockd -f /srv/sockd.conf -p /dev/null
+bash
