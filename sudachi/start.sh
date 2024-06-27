e1332b86a41582efe1a54cf728246ad9c466fad6
xatier <xatierlike@gmail.com>
2024-06-26 22:59:49 -0500

    chore(sudachi): bump dictionary to 20240409

--- sudachi/start.sh
+++ sudachi/start.sh
@@ -10,10 +10,10 @@ sudo pacman -Syuu --noconfirm --needed jre-openjdk-headless unzip
 wget -q https://github.com/WorksApplications/Sudachi/releases/download/v0.7.3/sudachi-0.7.3-executable.zip
 
 # http://sudachi.s3-website-ap-northeast-1.amazonaws.com/sudachidict/
-wget -q http://sudachi.s3-website-ap-northeast-1.amazonaws.com/sudachidict/sudachi-dictionary-20240109-full.zip
+wget -q http://sudachi.s3-website-ap-northeast-1.amazonaws.com/sudachidict/sudachi-dictionary-20240409-full.zip
 
 unzip sudachi-0.7.3-executable.zip
-unzip sudachi-dictionary-20240109-full.zip
+unzip sudachi-dictionary-20240409-full.zip
 
 sudo archlinux-java set java-22-openjdk
 archlinux-java status
