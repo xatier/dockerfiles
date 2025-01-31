0e35e6482d8d21ca6206dc605c746f98a76af77d
xatier <xatierlike@gmail.com>
2025-01-31 05:37:53 -0600

    chore(sudachi): bump to 0.7.5 and bump dictionary to 20250129
    * bump jre to java 23

--- sudachi/start.sh
+++ sudachi/start.sh
@@ -7,13 +7,13 @@ set -euxo pipefail
 sudo pacman -Syuu --noconfirm --needed jre-openjdk-headless unzip
 
 # https://github.com/WorksApplications/Sudachi/releases/
-wget -q https://github.com/WorksApplications/Sudachi/releases/download/v0.7.3/sudachi-0.7.3-executable.zip
+wget -q https://github.com/WorksApplications/Sudachi/releases/download/v0.7.5/sudachi-0.7.5-executable.zip
 
 # http://sudachi.s3-website-ap-northeast-1.amazonaws.com/sudachidict/
-wget -q http://sudachi.s3-website-ap-northeast-1.amazonaws.com/sudachidict/sudachi-dictionary-20240409-full.zip
+wget -q http://sudachi.s3-website-ap-northeast-1.amazonaws.com/sudachidict/sudachi-dictionary-20250129-full.zip
 
-unzip sudachi-0.7.3-executable.zip
-unzip sudachi-dictionary-20240409-full.zip
+unzip sudachi-0.7.5-executable.zip
+unzip sudachi-dictionary-20250129-full.zip
 
-sudo archlinux-java set java-22-openjdk
+sudo archlinux-java set java-23-openjdk
 archlinux-java status
